d3b2979880117cf0d28a2e95af96b9ea1f02afe0
lorance19 <46634665+lorance19@users.noreply.github.com>
2021-04-26 19:28:36 -0700

    testing

--- manage.py
+++ manage.py
@@ -53,7 +53,7 @@ while True:
     checkQuotaCount()
     ethData = checkPrice();
     ethamount = float( ethData['amount'])
-    print(ethamount)
+    print('Current Amount'+ethamount+ ', Desired Amount '+ DesireAmount)
     message_high = "ETH-USD value is supress "+ str(DesireAmount)+ '$.'
     message_low = "ETH-USD value is drop to "+ str(DesireAmount)+'$.'
 
